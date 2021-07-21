c2dcae226702e5fde328ae6814f4204092134ba9
Simo Droussi <39590663+simodroussi@users.noreply.github.com>
2021-07-21 10:36:34 +0000

    Initial commit

---
+++ scenes/main.js
@@ -0,0 +1,34 @@
+const player = add([
+  sprite('player'),
+  scale(0.5),
+  pos(20,20),
+  body()
+  ])
+
+const MOVE_SPEED = 200
+
+keyDown('right', ()=> {
+  player.move(MOVE_SPEED, 0)
+})
+
+keyDown('left', ()=> {
+  player.move(-MOVE_SPEED, 0)
+})
+
+  addLevel([
+    '     ',
+    '  @  ',
+    '     ',
+    '     ',
+    'xxxxx'
+  ],{
+    width: 40,
+    height: 40,
+    'x': [sprite('ground'), solid()],
+    '@': [sprite('enemy'), scale(0.5), body(), 'dangerous']
+
+  })
+
+  player.collides('dangerous', () => {
+    destroy(player)
+  })
